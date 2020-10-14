41e10806d952accef48e9f2a09e503c1e24749e6
cleave <55739495+martincleave@users.noreply.github.com>
2020-10-14 13:10:30 +0100

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,13 +1,13 @@
 <?php
 echo '
+        <a href="https://www.totallyplc.com" class="ui medium"><img src="assets/images/totally_lg.PNG" style="float: right; margin-right: 1%;" width="15%"></a>
 <div class="ui  vertical footer segment" style="margin-top: 8em; background-color: #00887A">
     <div class="ui center aligned container">
         <div class="ui horizontal inverted  small divided link list ">
             <a class="item white" href="sitemap.php">Site Map</a>
             <a class="item white" href="contact-us.php">Contact Us</a>
             <a class="item white" href="privacy-policy.php">Privacy Policy &amp; Modern Slavery Statement</a>
-            <a href="https://www.totallyplc.com" class="ui medium"><img src="assets/images/totally_lg.PNG" style="float: right; margin-right: 2%;" width="10%"></a>
-            
+          
         </div>
     </div>
 </div>';
